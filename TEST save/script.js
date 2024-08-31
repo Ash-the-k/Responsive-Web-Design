4f392a4191c7e89dc1974c5199ee385ad94a1449
Ash4-k <ashlesh.kanchan@gmail.com>
2024-08-31 23:00:21 +0530

    2.1 2.2 Completed

---
+++ TEST save/script.js
@@ -0,0 +1,11 @@
+// window.addEventListener('scroll', function() {
+//     let sections = document.querySelectorAll('section');
+//     sections.forEach(section => {
+//         let rect = section.getBoundingClientRect();
+//         if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+//             section.style.backgroundColor = 'lightblue';
+//         } else {
+//             section.style.backgroundColor = 'lightgrey';
+//         }
+//     });
+// });
